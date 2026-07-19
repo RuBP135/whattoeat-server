eedcd9d809a9f23242eff0b1be3b81b298dbac32
R810@foxmail.com <R810@foxmail.com>
2026-07-20 01:06:29 +0800

    first commit

---
+++ src/main/java/com/rubp/whattoeat/server/WhattoeatServerApplication.java
@@ -0,0 +1,13 @@
+package com.rubp.whattoeat.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhattoeatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhattoeatServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rubp/whattoeat/server/WhattoeatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubp.whattoeat.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhattoeatServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
