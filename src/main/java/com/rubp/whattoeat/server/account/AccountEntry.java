eec4b9451f8022845b4ff454837cc05316911e4d
RuBP135 <R810@foxmail.com>
2026-09-01 01:22:10 +0800

    feat(acounte-entry): 增加了acount-status、created-at

--- src/main/java/com/rubp/whattoeat/server/account/AccountEntry.java
+++ src/main/java/com/rubp/whattoeat/server/account/AccountEntry.java
@@ -16,5 +16,45 @@ public class AccountEntry {
     @Column(nullable = false, unique = true, updatable = false, length = 10)
     private String uid;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AccountStatus status;
 
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+
+    protected AccountEntry() { }
+
+    public AccountEntry(String uid, AccountStatus status, Instant createdAt) {
+        this.uid = uid;
+        this.status = status;
+        this.createdAt = createdAt;
+    }
+
+
+    public void active() {
+        this.status = AccountStatus.ACTIVE;
+    }
+
+    public void disable() {
+        this.status = AccountStatus.DISABLED;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public AccountStatus getStatus() {
+        return status;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
 }
