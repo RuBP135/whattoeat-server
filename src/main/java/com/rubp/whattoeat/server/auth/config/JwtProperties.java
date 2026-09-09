220fb1507a0c54fa023f6af6998be96066ac4b7a
RuBP135 <R810@foxmail.com>
2026-09-09 14:24:07 +0800

    fix(JwtProperties): 将@Valid改为@Validated，去掉@Positive

--- src/main/java/com/rubp/whattoeat/server/auth/config/JwtProperties.java
+++ src/main/java/com/rubp/whattoeat/server/auth/config/JwtProperties.java
@@ -1,18 +1,17 @@
 package com.rubp.whattoeat.server.auth.config;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 import java.time.Duration;
 
-@Valid
+@Validated
 @ConfigurationProperties(prefix = "jwt")
 public record JwtProperties (
         @NotBlank String privateKeyBase64,
         @NotBlank String publicKeyBase64,
-        @NotNull @Positive Duration accessTokenTtl,
-        @NotNull @Positive Duration refreshTokenTtl
+        @NotNull Duration accessTokenTtl,
+        @NotNull Duration refreshTokenTtl
 ) { }
