efa0a960d98c590feb89101867f7badb3a900290
RuBP135 <R810@foxmail.com>
2026-09-14 13:16:57 +0800

    feat(AnonymouosAuthService): 增加创建匿名用户的方法

--- src/main/java/com/rubp/whattoeat/server/auth/service/AnonymousAuthService.java
+++ src/main/java/com/rubp/whattoeat/server/auth/service/AnonymousAuthService.java
@@ -1,9 +1,30 @@
 package com.rubp.whattoeat.server.auth.service;
 
+import com.rubp.whattoeat.server.account.AccountEntry;
+import com.rubp.whattoeat.server.account.AccountService;
+import com.rubp.whattoeat.server.account.model.AccountStatus;
+import com.rubp.whattoeat.server.auth.model.CreateAnonymousAccountResponse;
+import com.rubp.whattoeat.server.auth.model.JwtTokenType;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AnonymousAuthService {
 
+    private final AccountService accountService;
+    private final JwtTokenService jwtTokenService;
 
+    public AnonymousAuthService(AccountService accountService, JwtTokenService jwtTokenService) {
+        this.accountService = accountService;
+        this.jwtTokenService = jwtTokenService;
+    }
+
+
+    public CreateAnonymousAccountResponse createAnonymousAccount(){
+        AccountEntry account = accountService.createAnonymousAccount();
+        return new CreateAnonymousAccountResponse(
+                jwtTokenService.createToken(account.getUid(), JwtTokenType.ACCESS),
+                jwtTokenService.createToken(account.getUid(), JwtTokenType.REFRESH),
+                account.getUid()
+        );
+    }
 }
